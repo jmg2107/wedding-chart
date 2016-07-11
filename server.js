40d06cac604afd61de52f70a152f58173695c752
jmg2107 <jenjengoo@gmail.com>
2016-07-11 14:21:14 -0700

    served up static files for server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var handler = require('./server/handler.js');
 
 var app = express();
 
-app.get('/', handler.renderIndex);
+app.use('/', express.static(__dirname + '/public/client'));
+
+
+app.get('/', function(req,res){
+  console.log("something");
+
+});
 
 
 module.exports = app;
