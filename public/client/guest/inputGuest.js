b72987c8cc979f562063176498eced8fb39e7044
jmg2107 <jenjengoo@gmail.com>
2016-07-11 19:02:51 -0700

    put in the hooks on the front end to input the guest to the server

--- public/client/guest/inputGuest.js
+++ public/client/guest/inputGuest.js
@@ -1,11 +1,31 @@
 angular.module('seating.guests',[])
 
-.controller('GuestController', function($scope){
+.controller('GuestController', function($scope, $http){
   $scope.guests = {
-    name: ""
+    name: "testname"
   };
-  $scope.input = function(){
-
+  $scope.input = function(name){
+    $scope.guests.name = name;
   }
+  $scope.send = function(){
+     return $http({
+      method: 'POST',
+      url: '/api/users',
+      data: $scope.guests
+    })
+    .then(function () {
+      console.log("created new Guest");
+    });
+  };
+  $scope.display = function(){
+    return $http({
+      method: 'GET',
+      url: '/api/users',
+    })
+    .then(function (res) {
+      console.log("res data is ", res.data);
+      return res;
+    });
+  };
 
 });
